c93722518d35f76ddee7b1c2663faac135d49e9a
goforsdream <916974727@qq.com>
2021-04-15 19:57:49 +0800

    second commit

--- src/main/java/com/atguigu/GitTest/MyCommit.java
+++ src/main/java/com/atguigu/GitTest/MyCommit.java
@@ -3,6 +3,12 @@ package com.atguigu.GitTest;
 public class MyCommit {
     public static void main(String[] args) {
         System.out.println("hello  i am  git ");
-        System.out.println("君不见黄河之水天上来，奔流到海不复回");
+        System.out.println("明月几时有？把酒问青天。" +
+                "不知天上宫阙，今夕是何年。我欲乘风归去，又恐琼楼玉宇，高处不胜寒。" +
+                "起舞弄清影，何似在人间。\n" +
+                "转朱阁，低绮户，照无眠。" +
+                "不应有恨，何事长向别时圆？人有悲欢离合，月有阴晴圆缺，此事古难全。" +
+                "但愿人长久，千里共婵娟。");
+        System.out.println("I'm master");
     }
 }

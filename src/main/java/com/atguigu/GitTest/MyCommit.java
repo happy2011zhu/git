bb7cf4e6440ed189527610fc985334bda5777159
goforsdream <916974727@qq.com>
2021-04-15 19:51:14 +0800

    second commit

---
+++ src/main/java/com/atguigu/GitTest/MyCommit.java
@@ -0,0 +1,8 @@
+package com.atguigu.GitTest;
+
+public class MyCommit {
+    public static void main(String[] args) {
+        System.out.println("hello  i am  git ");
+        System.out.println("君不见黄河之水天上来，奔流到海不复回");
+    }
+}
